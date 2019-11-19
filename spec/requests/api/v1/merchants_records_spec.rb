6285d669948a737f8d44ee818bf0a83f5ec6fcf7
Alice Post <alicep2322@gmail.com>
2019-11-19 16:45:42 -0700

    Add merchant show record endpoint

--- spec/requests/api/v1/merchants_records_spec.rb
+++ spec/requests/api/v1/merchants_records_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 describe "Merchants Records API" do
-  it "sends a list of merchants" do
+  before(:each) do
     create_list(:merchant, 3)
+  end
 
+  after(:all) do
+    Merchant.all.delete_all
+  end
+
+  it "sends a list of merchants" do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
@@ -11,5 +17,16 @@ describe "Merchants Records API" do
     merchants = JSON.parse(response.body)
 
     expect(merchants.count).to eq 3
+  end
+
+  it "shows a single merchant" do
+    id = Merchant.first.id
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
   end
 end
