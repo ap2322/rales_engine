81075f4cdb412edead269783036e3f9f95dea410
Alice Post <alicep2322@gmail.com>
2019-11-20 18:50:46 -0700

    Update random merchant method to not error out if someone destroys a merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -16,7 +16,7 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def random
-    random_id = rand(Merchant.first.id..Merchant.last.id)
+    random_id = Merchant.ids.sample
     render json: MerchantSerializer.new(Merchant.find(random_id))
   end
 end
