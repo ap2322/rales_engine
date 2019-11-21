998069a07837f0e3683a17970641e7526d67d3bc
Alice Post <alicep2322@gmail.com>
2019-11-20 18:51:43 -0700

    Start making dependent destroy associations to reset testing database

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,5 +8,25 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it {should have_many :items}
     it {should have_many :invoices}
+    it {should have_many(:invoice_items).through(:invoices)}
+    it {should have_many(:transactions).through(:invoices)}
+  end
+
+  describe 'dependencies' do
+    it 'deletes all items when a merchant is destroyed' do
+      item = create(:item)
+      merchant = Merchant.find(item.merchant_id)
+
+      Merchant.destroy(merchant.id)
+      expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'deletes all invoices when a merchant is destroyed' do
+      merchant = create(:merchant, :with_invoices, :invoice_count => 1)
+      invoice = merchant.invoices.last
+
+      Merchant.destroy(merchant.id)
+      expect{Invoice.find(invoice.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
   end
 end
