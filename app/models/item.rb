d17cf5a9d225bcb699d6ddd5f0fb320e883e3227
Alice Post <alicep2322@gmail.com>
2019-11-21 15:15:41 -0700

    Default sort items by id to pass spec harness

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
   has_many :invoice_items
 
   validates_presence_of :name, :description, :unit_price
+
+  default_scope { order(id: :asc) }
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,11 +2,8 @@ class ItemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :description, :merchant_id
 
-  set_id :merchant_id
-
   attributes :unit_price do |item|
     item.unit_price.to_s
   end
 
-  belongs_to :merchant
 end
