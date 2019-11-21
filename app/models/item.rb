0d27ee5376ee200336e9e0180d855f95e01272e4
Alice Post <alicep2322@gmail.com>
2019-11-21 15:04:55 -0700

    Add most revenue for top x items method and associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
 
   validates_presence_of :name, :description, :unit_price
+
+  def self.most_revenue(x)
+    joins(:transactions)
+    .select("items.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+    .group(:id)
+    .where(transactions:{result: 'success'})
+    .order("revenue desc")
+    .limit(x)
+  end
 end
