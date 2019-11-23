194e60b339781ba0be22b8ac522a717198461295
Alice Post <alicep2322@gmail.com>
2019-11-23 14:28:56 -0700

    Update transaction test to match spec harness

--- spec/requests/api/v1/record_endpoints/transaction_record_endpoints_spec.rb
+++ spec/requests/api/v1/record_endpoints/transaction_record_endpoints_spec.rb
@@ -36,7 +36,7 @@ describe "Transaction Records API" do
     invoice = create(:invoice)
     transaction = Transaction.create!(credit_card_number: 4844518708741275, credit_card_expiration_date: nil, result: 'success', invoice_id: invoice.id, created_at: "2012-03-27 14:53:59", updated_at: "2015-03-27 14:53:59")
 
-    attributes = ['invoice_id', 'credit_card_number','result']
+    attributes = ['credit_card_number','result']
 
     transaction_group.push(transaction)
 
@@ -46,16 +46,16 @@ describe "Transaction Records API" do
         json_transaction = JSON.parse(response.body)
 
         expect(response).to be_successful
-        expect(json_transaction["data"]["attributes"][attribute]).to eq(transaction.send(attribute))
+        expect(json_transaction["data"]["attributes"][attribute]).to eq(transaction.send(attribute).to_s)
       end
     end
 
-    date_attributes = [ 'credit_card_expiration_date', 'created_at', 'updated_at']
+    other_attributes = [ 'created_at', 'updated_at', 'invoice_id' ]
 
     transaction_group = [transaction_group.first, transaction]
 
     transaction_group.each do |transaction|
-      date_attributes.each do |attribute|
+      other_attributes.each do |attribute|
         unless transaction.send(attribute).nil?
           get "/api/v1/transactions/find?#{attribute}=#{transaction.send(attribute)}"
           json_transaction = JSON.parse(response.body)
@@ -73,7 +73,7 @@ describe "Transaction Records API" do
     invoice = create(:invoice)
     indv_transaction = Transaction.create!(credit_card_number: 4844518708741275, credit_card_expiration_date: nil, result: 'success', invoice_id: invoice.id, created_at: "2012-03-27 14:53:59", updated_at: "2015-03-27 14:53:59")
 
-    attributes = ['invoice_id', 'credit_card_number','result']
+    attributes = ['credit_card_number','result']
 
     transaction_group.push(indv_transaction)
     transaction_group.each do |transaction|
@@ -83,7 +83,7 @@ describe "Transaction Records API" do
 
         expect(response).to be_successful
         expect(json_transaction["data"]).to be_instance_of(Array)
-        expect(json_transaction["data"].first["attributes"][attribute]).to eq(transaction.send(attribute))
+        expect(json_transaction["data"].first["attributes"][attribute]).to eq(transaction.send(attribute).to_s)
 
         # indv vs group check
         if transaction.id == indv_transaction.id
@@ -94,11 +94,11 @@ describe "Transaction Records API" do
       end
     end
 
-    date_attributes = [ 'credit_card_expiration_date', 'created_at', 'updated_at']
+    other_attributes = [ 'created_at', 'updated_at', 'invoice_id' ]
     transaction_group = [transaction_group.first, indv_transaction]
 
     transaction_group.each do |transaction|
-      date_attributes.each do |attribute|
+      other_attributes.each do |attribute|
         unless transaction.send(attribute).nil?
           get "/api/v1/transactions/find_all?#{attribute}=#{transaction.send(attribute)}"
           json_transaction = JSON.parse(response.body)
@@ -115,50 +115,16 @@ describe "Transaction Records API" do
         end
       end
     end
-
-    # get "/api/v1/items/find_all?created_at=#{item.created_at}"
-    # json_items = JSON.parse(response.body)
-    #
-    # expect(response).to be_successful
-    # expect(json_items["data"].count).to eq(1)
-    # expect(json_items["data"]).to be_instance_of(Array)
-    # expect(json_items["data"].first["id"]).to eq(item.id.to_s)
-    #
-    # get "/api/v1/items/find_all?updated_at=#{item.updated_at}"
-    # json_items = JSON.parse(response.body)
-    #
-    # expect(response).to be_successful
-    # expect(json_items["data"].count).to eq(1)
-    # expect(json_items["data"]).to be_instance_of(Array)
-    # expect(json_items["data"].first["id"]).to eq(item.id.to_s)
-    #
-    # get "/api/v1/items/find_all?merchant_id=#{item.merchant_id}"
-    # json_items = JSON.parse(response.body)
-    #
-    # expect(response).to be_successful
-    # expect(json_items["data"].count).to eq(1)
-    # expect(json_items["data"]).to be_instance_of(Array)
-    # expect(json_items["data"].first["id"]).to eq(item.id.to_s)
-    #
-    # get "/api/v1/items/find_all?merchant_id=#{merchant_1.id}"
-    # json_items = JSON.parse(response.body)
-    #
-    # expect(response).to be_successful
-    # expect(json_items["data"].count).to eq(3)
-    # expect(json_items["data"]).to be_instance_of(Array)
-    # item_in_collection = items.first
-    # expect(json_items["data"].first["id"]).to eq(item_in_collection.id.to_s)
-
   end
 
-  xit "returns a random record" do
-    create_list(:item, 3)
-    id_low = Item.first.id
-    id_high = Item.last.id
+  it "returns a random record" do
+    create_list(:transaction, 3)
+    id_low = Transaction.first.id
+    id_high = Transaction.last.id
 
-    get "/api/v1/items/random"
-    json_customer = JSON.parse(response.body)
+    get "/api/v1/transactions/random"
+    json_transaction = JSON.parse(response.body)
     expect(response).to be_successful
-    expect(json_customer["data"]["id"].to_i).to be_between(id_low, id_high)
+    expect(json_transaction["data"]["id"].to_i).to be_between(id_low, id_high)
   end
 end
