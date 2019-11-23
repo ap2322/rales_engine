9010d4f8da17d051b44eed396bcf8c8e698b2b0c
Alice Post <alicep2322@gmail.com>
2019-11-23 14:07:44 -0700

    Add find_all query to transactions_records test

--- spec/requests/api/v1/record_endpoints/transaction_record_endpoints_spec.rb
+++ spec/requests/api/v1/record_endpoints/transaction_record_endpoints_spec.rb
@@ -67,68 +67,87 @@ describe "Transaction Records API" do
     end
   end
 
-  xit "returns all items that match a specific query parameter" do
-    merchant_1 = create(:merchant)
-    items = create_list(:item, 3, name: "Thingy", description: "It's a thing", unit_price: 3.4, merchant_id: merchant_1.id)
-    merchant = create(:merchant)
-    item = Item.create!(name: "Best Thing Ever", description: "Seriously, it's the best thing ever", unit_price: 5.5, merchant_id: merchant.id, created_at: "2012-03-27 14:53:59", updated_at: "2015-03-27 14:53:59")
+  it "returns all transactions that match a specific query parameter" do
+    invoice_1 = create(:invoice)
+    transaction_group = create_list(:transaction, 3, :credit_card_number => 4000000000000000, invoice_id: invoice_1.id, result: 0)
+    invoice = create(:invoice)
+    indv_transaction = Transaction.create!(credit_card_number: 4844518708741275, credit_card_expiration_date: nil, result: 'success', invoice_id: invoice.id, created_at: "2012-03-27 14:53:59", updated_at: "2015-03-27 14:53:59")
 
-    get "/api/v1/items/find_all?name=Thingy"
-    json_items = JSON.parse(response.body)
+    attributes = ['invoice_id', 'credit_card_number','result']
 
-    expect(response).to be_successful
-    expect(json_items["data"].count).to eq(3)
-    expect(json_items["data"]).to be_instance_of(Array)
-    expect(json_items["data"].first["attributes"]["name"]).to eq("Thingy")
+    transaction_group.push(indv_transaction)
+    transaction_group.each do |transaction|
+      attributes.each do |attribute|
+        get "/api/v1/transactions/find_all?#{attribute}=#{transaction.send(attribute)}"
+        json_transaction = JSON.parse(response.body)
 
-    get "/api/v1/items/find_all?description=It's a thing"
-    json_items = JSON.parse(response.body)
+        expect(response).to be_successful
+        expect(json_transaction["data"]).to be_instance_of(Array)
+        expect(json_transaction["data"].first["attributes"][attribute]).to eq(transaction.send(attribute))
 
-    expect(response).to be_successful
-    expect(json_items["data"].count).to eq(3)
-    expect(json_items["data"]).to be_instance_of(Array)
-    expect(json_items["data"].first["attributes"]["description"]).to eq("It's a thing")
+        # indv vs group check
+        if transaction.id == indv_transaction.id
+          expect(json_transaction["data"].count).to eq(1)
+        else
+          expect(json_transaction["data"].count).to eq(3)
+        end
+      end
+    end
 
-    get "/api/v1/items/find_all?id=#{item.id}"
-    json_items = JSON.parse(response.body)
+    date_attributes = [ 'credit_card_expiration_date', 'created_at', 'updated_at']
+    transaction_group = [transaction_group.first, indv_transaction]
 
-    expect(response).to be_successful
-    expect(json_items["data"].count).to eq(1)
-    expect(json_items["data"]).to be_instance_of(Array)
-    expect(json_items["data"].first["id"]).to eq(item.id.to_s)
+    transaction_group.each do |transaction|
+      date_attributes.each do |attribute|
+        unless transaction.send(attribute).nil?
+          get "/api/v1/transactions/find_all?#{attribute}=#{transaction.send(attribute)}"
+          json_transaction = JSON.parse(response.body)
 
-    get "/api/v1/items/find_all?created_at=#{item.created_at}"
-    json_items = JSON.parse(response.body)
+          expect(response).to be_successful
+          expect(json_transaction["data"].first["id"].to_i).to eq(transaction.id)
 
-    expect(response).to be_successful
-    expect(json_items["data"].count).to eq(1)
-    expect(json_items["data"]).to be_instance_of(Array)
-    expect(json_items["data"].first["id"]).to eq(item.id.to_s)
+          # indv vs group check
+          if transaction.id == indv_transaction.id
+            expect(json_transaction["data"].count).to eq(1)
+          else
+            expect(json_transaction["data"].count).to eq(3)
+          end
+        end
+      end
+    end
 
-    get "/api/v1/items/find_all?updated_at=#{item.updated_at}"
-    json_items = JSON.parse(response.body)
-
-    expect(response).to be_successful
-    expect(json_items["data"].count).to eq(1)
-    expect(json_items["data"]).to be_instance_of(Array)
-    expect(json_items["data"].first["id"]).to eq(item.id.to_s)
-
-    get "/api/v1/items/find_all?merchant_id=#{item.merchant_id}"
-    json_items = JSON.parse(response.body)
-
-    expect(response).to be_successful
-    expect(json_items["data"].count).to eq(1)
-    expect(json_items["data"]).to be_instance_of(Array)
-    expect(json_items["data"].first["id"]).to eq(item.id.to_s)
-
-    get "/api/v1/items/find_all?merchant_id=#{merchant_1.id}"
-    json_items = JSON.parse(response.body)
-
-    expect(response).to be_successful
-    expect(json_items["data"].count).to eq(3)
-    expect(json_items["data"]).to be_instance_of(Array)
-    item_in_collection = items.first
-    expect(json_items["data"].first["id"]).to eq(item_in_collection.id.to_s)
+    # get "/api/v1/items/find_all?created_at=#{item.created_at}"
+    # json_items = JSON.parse(response.body)
+    #
+    # expect(response).to be_successful
+    # expect(json_items["data"].count).to eq(1)
+    # expect(json_items["data"]).to be_instance_of(Array)
+    # expect(json_items["data"].first["id"]).to eq(item.id.to_s)
+    #
+    # get "/api/v1/items/find_all?updated_at=#{item.updated_at}"
+    # json_items = JSON.parse(response.body)
+    #
+    # expect(response).to be_successful
+    # expect(json_items["data"].count).to eq(1)
+    # expect(json_items["data"]).to be_instance_of(Array)
+    # expect(json_items["data"].first["id"]).to eq(item.id.to_s)
+    #
+    # get "/api/v1/items/find_all?merchant_id=#{item.merchant_id}"
+    # json_items = JSON.parse(response.body)
+    #
+    # expect(response).to be_successful
+    # expect(json_items["data"].count).to eq(1)
+    # expect(json_items["data"]).to be_instance_of(Array)
+    # expect(json_items["data"].first["id"]).to eq(item.id.to_s)
+    #
+    # get "/api/v1/items/find_all?merchant_id=#{merchant_1.id}"
+    # json_items = JSON.parse(response.body)
+    #
+    # expect(response).to be_successful
+    # expect(json_items["data"].count).to eq(3)
+    # expect(json_items["data"]).to be_instance_of(Array)
+    # item_in_collection = items.first
+    # expect(json_items["data"].first["id"]).to eq(item_in_collection.id.to_s)
 
   end
 
