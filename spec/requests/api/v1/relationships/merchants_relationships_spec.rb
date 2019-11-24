a3106e6b12c40147a87df3c051f0d0b85b595608
Alice Post <alicep2322@gmail.com>
2019-11-23 22:19:44 -0700

    Add test to find invoices for a merchant endpoint

--- spec/requests/api/v1/relationships/merchants_relationships_spec.rb
+++ spec/requests/api/v1/relationships/merchants_relationships_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
-# GET /api/v1/merchants/:id/items returns a collection of items associated with that merchant
+# GET /api/v1/merchants/:id/invoices returns a collection of invoices associated with that merchant from their known orders
+
 describe "Merchants Relationships API" do
 
   it 'returns a collection of items associated with a merchant' do
@@ -10,7 +11,7 @@ describe "Merchants Relationships API" do
       get "/api/v1/merchants/#{merch.id}/items"
       expect(response).to be_successful
       items = JSON.parse(response.body)
-      
+
       items_belong_to_merchant = items["data"].all? do |item|
         item['attributes']['merchant_id'] == merch.id
       end
@@ -20,6 +21,21 @@ describe "Merchants Relationships API" do
         extra_item.id == item["attributes"]["id"]
       end
       expect(extra).to be_falsy
+    end
+  end
+
+  it 'returns a collection of invoices associated with that merchant' do
+    merchants = create_list(:merchant, 3, :with_invoices)
+    extra_invoice = create(:invoice)
+    merchants.each do |merch|
+      get "/api/v1/merchants/#{merch.id}/invoices"
+      expect(response).to be_successful
+      invoices = JSON.parse(response.body)
+
+      invoices_belong_to_merchant = invoices["data"].all? do |invoice|
+        invoice['attributes']['merchant_id'] == merch.id
+      end
+      expect(invoices_belong_to_merchant).to be_truthy
     end
   end
 end
