b2b803eb8381dd71e568227ed08e16fd7334dd9f
Alice Post <alicep2322@gmail.com>
2019-11-28 11:49:29 -0700

    Setup revenue by date tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -61,5 +61,42 @@ RSpec.describe Merchant, type: :model do
       top_two = Merchant.most_revenue(2)
       expect(top_two).to eq([merchant_3, merchant_2])
     end
+
+    it 'returns the total revenue for date x across all merchants' do
+      Merchant.all.destroy_all
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      merchant_3 = create(:merchant)
+
+      create_list(:invoice, 3, merchant_id: merchant_1.id, created_at: '2012-03-27 14:54:09 UTC')
+      merchant_1.invoices.each do |invoice|
+        item = create(:item, merchant_id: merchant_1.id)
+        create(:invoice_item, invoice_id: invoice.id, item_id: item.id, unit_price: 3.0, quantity: 2, created_at: '2012-03-27 14:54:09 UTC')
+        create(:transaction, invoice_id: invoice.id, created_at: '2012-03-27 14:54:09 UTC')
+      end
+
+      create_list(:invoice, 3, merchant_id: merchant_2.id, created_at: '2012-03-08 20:54:10 UTC')
+      merchant_2.invoices.each do |invoice|
+        item = create(:item, merchant_id: merchant_2.id)
+        create(:invoice_item, invoice_id: invoice.id, item_id: item.id, unit_price: 4.0, quantity: 2, created_at: '2012-03-08 20:54:10 UTC')
+        create(:transaction, invoice_id: invoice.id, created_at: '2012-03-08 20:54:10 UTC')
+      end
+
+      create_list(:invoice, 3, merchant_id: merchant_3.id, created_at: '2012-03-10 10:54:10 UTC')
+      merchant_3.invoices.each do |invoice|
+        item = create(:item, merchant_id: merchant_3.id)
+        create(:invoice_item, invoice_id: invoice.id, item_id: item.id, unit_price: 5.0, quantity: 2, created_at: '2012-03-10 10:54:10 UTC')
+        create(:transaction, invoice_id: invoice.id, created_at: '2012-03-10 10:54:10 UTC')
+      end
+
+      date_1 = '2012-03-27'
+      date_2 = '2012-03-10'
+
+      total_date_1 = 18
+      total_date_2 = 30
+
+      expect(Merchant.revenue(date_1)).to eq(total_date_1)
+      expect(Merchant.revenue(date_2)).to eq(total_date_2)
+    end
   end
 end
--- spec/requests/api/v1/business_intelligence/merchants_spec.rb
+++ spec/requests/api/v1/business_intelligence/merchants_spec.rb
@@ -5,9 +5,7 @@ describe "Merchant's business intelligence API" do
   before(:each) do
     Merchant.all.destroy_all
   end
-# GET /api/v1/merchants/most_revenue?quantity=x returns the top x merchants ranked by total revenue
   it "returns the top x merchants ranked by revenue" do
-    Merchant.all.destroy_all
     merchant_1 = create(:merchant)
     merchant_2 = create(:merchant)
     merchant_3 = create(:merchant)
@@ -47,5 +45,38 @@ describe "Merchant's business intelligence API" do
     expect(top_1_merchant.id).to eq merchant_3.id
     expect(top_2_merchant.id).to eq merchant_2.id
 
+  end
+
+  # GET /api/v1/merchants/revenue?date=x returns the total revenue for date x across all merchants
+  it 'returns the total revenue for date x across all merchants' do
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    merchant_3 = create(:merchant)
+
+    create_list(:invoice, 3, merchant_id: merchant_1.id, created_at: '2012-03-27 14:54:09 UTC')
+    merchant_1.invoices.each do |invoice|
+      item = create(:item, merchant_id: merchant_1.id)
+      create(:invoice_item, invoice_id: invoice.id, item_id: item.id, unit_price: 3.0, quantity: 2, created_at: '2012-03-27 14:54:09 UTC')
+      create(:transaction, invoice_id: invoice.id, created_at: '2012-03-27 14:54:09 UTC')
+    end
+
+    create_list(:invoice, 3, merchant_id: merchant_2.id, created_at: '2012-03-08 20:54:10 UTC')
+    merchant_2.invoices.each do |invoice|
+      item = create(:item, merchant_id: merchant_2.id)
+      create(:invoice_item, invoice_id: invoice.id, item_id: item.id, unit_price: 4.0, quantity: 2, created_at: '2012-03-08 20:54:10 UTC')
+      create(:transaction, invoice_id: invoice.id, created_at: '2012-03-08 20:54:10 UTC')
+    end
+
+    create_list(:invoice, 3, merchant_id: merchant_3.id, created_at: '2012-03-10 10:54:10 UTC')
+    merchant_3.invoices.each do |invoice|
+      item = create(:item, merchant_id: merchant_3.id)
+      create(:invoice_item, invoice_id: invoice.id, item_id: item.id, unit_price: 5.0, quantity: 2, created_at: '2012-03-10 10:54:10 UTC')
+      create(:transaction, invoice_id: invoice.id, created_at: '2012-03-10 10:54:10 UTC')
+    end
+
+
+    get "/api/v1/merchants/revenue?date=#{x}"
+
+    expect(response).to be_successful
   end
 end
