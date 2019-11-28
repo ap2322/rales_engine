abb309cb51b44038fcfc5503ba165efa251212cc
Alice Post <alicep2322@gmail.com>
2019-11-28 10:15:56 -0700

    Continue dependent destroy chain through resources

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,9 +1,9 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
 
   enum status: %w(pending shipped)
 
