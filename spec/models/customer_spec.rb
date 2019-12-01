2b39eb60c298c9ec579e8d22a7ad6736d1b900b6
Alice Post <alicep2322@gmail.com>
2019-12-01 16:43:51 -0700

    Add test for customer model method to find favorite merchant

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe Customer, type: :model do
   describe 'relationships' do
     it {should have_many :invoices}
   end
+
+  describe 'methods' do
+    it 'returns the merchant for a customer where they had the most successful transactions' do
+      customer = create(:customer)
+      merchants = create_list(:merchant, 3)
+
+      merchants.each do |merchant|
+        invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+        create(:transaction, invoice_id: invoice.id)
+      end
+
+      # more_invoices_and_transactions for the last merchant
+      second_merchant = merchants[1]
+      invoices = create_list(:invoice, 2, merchant_id: second_merchant.id, customer_id: customer.id)
+      invoices.each { |invoice| create(:transaction, invoice_id: invoice.id)}
+      expect(customer.favorite_merchant).to eq(second_merchant)
+    end
+  end
 end
