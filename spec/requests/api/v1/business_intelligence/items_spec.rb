ffbe48d9fdac16b0cd190ef46dcb86f1133b4a4f
Alice Post <alicep2322@gmail.com>
2019-12-01 15:02:56 -0700

    Add endpoint test for item's day with the most sales

--- spec/requests/api/v1/business_intelligence/items_spec.rb
+++ spec/requests/api/v1/business_intelligence/items_spec.rb
@@ -38,4 +38,32 @@ describe 'Items business intelligence endpoints' do
     expect(top_2_item.id).to eq item_3.id
 
   end
+
+  # GET /api/v1/items/:id/best_day returns the date with the most sales for the given item using the invoice date. If there are multiple days with equal number of sales, return the most recent day.
+  it 'returns the date with the most sales for the given item using the invoice date' do
+    item_1 = create(:item, unit_price: 2.1)
+    item_2 = create(:item, unit_price: 3.5)
+
+    invoice_1 = create(:invoice, merchant_id: item_1.merchant.id, created_at: '2012-03-27 14:54:09 UTC')
+    invoice_2 = create(:invoice, merchant_id: item_2.merchant.id, created_at: '2012-03-08 20:54:10 UTC')
+    invoice_3 = create(:invoice, merchant_id: item_1.merchant.id, created_at: '2012-03-27 14:54:09 UTC')
+
+    create_list(:invoice_item, 3, invoice_id: invoice_1.id, item_id: item_1.id, unit_price: item_1.unit_price, quantity: 2, created_at: '2012-03-27 14:54:09 UTC')
+    create_list(:invoice_item, 3, invoice_id: invoice_2.id, item_id: item_2.id, unit_price: item_2.unit_price, quantity: 4, created_at: '2012-03-08 20:54:10 UTC')
+    create_list(:invoice_item, 3, invoice_id: invoice_3.id, item_id: item_1.id, unit_price: item_1.unit_price, quantity: 3, created_at: '2012-03-27 14:54:09 UTC')
+
+    [invoice_1, invoice_2, invoice_3].each do |invoice|
+      create(:transaction, invoice_id: invoice.id)
+    end
+
+    get "/api/v1/items/#{item_1.id}/best_day"
+
+    expect(response).to be_successful
+
+    best_day_data = JSON.parse(response.body)
+
+    expect(best_day_data["data"]["attributes"]["best_day"]).to eq('2012-03-27')
+
+  end
+
 end
