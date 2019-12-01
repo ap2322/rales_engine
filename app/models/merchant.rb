5f1e17f310d09ed7a8637bd8a4a5222e3d3c6f69
Alice Post <alicep2322@gmail.com>
2019-11-30 22:06:11 -0700

    Add relationship to customer and instance method for favorite_customer

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,6 +5,7 @@ class Merchant < ApplicationRecord
   has_many :invoices, dependent: :destroy
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
 
   def self.most_revenue(x)
     joins(:invoice_items, :transactions)
@@ -26,5 +27,15 @@ class Merchant < ApplicationRecord
 
     # joins(:invoice_items, :transactions).select("sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue, invoices.created_at::timestamp::date").group("invoices.created_at::timestamp::date").where("invoices.created_at::timestamp::date = ?", date).sum("invoice_items.quantity * invoice_items.unit_price")
     # Merchant.joins(:invoice_items, "INNER JOIN transactions ON transactions.invoice_id = invoices.id").select("sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue, invoices.created_at::timestamp::date").merge(Transaction.unscoped.successful).group("invoices.created_at::timestamp::date").having("invoices.created_at::timestamp::date = ?", "2012-03-16")
+  end
+
+  def favorite_customer
+    Customer.joins(:transactions, :merchants)
+            .select("count(invoices.customer_id) as invoices_per_customer, customers.*, invoices.merchant_id")
+            .where(transactions: {result: 'success'}, merchants: {id: id})
+            .group("customers.id, invoices.merchant_id")
+            .order("invoices_per_customer desc")
+            .having(invoices: {merchant_id: id})
+            .first
   end
 end
