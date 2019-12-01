78b6d901b8bba3fea35712fdd6d4d91a0b411771
Alice Post <alicep2322@gmail.com>
2019-11-30 17:46:01 -0700

    Sum revenue across all merchants for a particular date

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -16,14 +16,12 @@ class Merchant < ApplicationRecord
   end
 
   def self.revenue(date)
-    joins(:invoice_items)
+    date_revenue_total = joins(:invoice_items)
     .select("sum(invoice_items.quantity * invoice_items.unit_price) as revenue, invoices.created_at::timestamp::date")
     .group("invoices.created_at::timestamp::date")
-    .where("invoices.created_at::timestamp::date = ?, #{date}")
+    .where("invoices.created_at::timestamp::date = ?", date)
+    .sum("invoice_items.quantity * invoice_items.unit_price")
 
-      # > created_at.beginning_of_day
-      # < created_at.end_of_day
-# Merchant.joins(:invoice_items).select("sum(invoice_items.quantity * invoice_items.unit_price) as revenue, invoices.created_at::timestamp::date").group("invoices.created_at::timestamp::date").order("invoices.created_at::timestamp::date")
-# SELECT sum(invoice_items.quantity * invoice_items.unit_price) as revenue, invoice_items.created_at::timestamp::date FROM "merchants" INNER JOIN "invoices" ON "invoices"."merchant_id" = "merchants"."id" INNER JOIN "invoice_items" ON "invoice_items"."invoice_id" = "invoices"."id" GROUP BY invoice_items.created_at::timestamp::date;
+    date_revenue_total[date.to_date]
   end
 end
